a9b9ac0011eb6d389b34f7f7980a776094a46e9c
Muhammad Khatri <khizar3840@gmail.com>
2019-05-03 23:43:22 -0500

    Close navbar on click

--- src/components/globalnavbar.js
+++ src/components/globalnavbar.js
@@ -14,14 +14,38 @@ export default class GlobalNavBar extends Component {
     super(props);
 
     this.toggle = this.toggle.bind(this);
+    this.close = this.close.bind(this);
+    this.handleClickOutside = this.handleClickOutside.bind(this);
     this.state = {
       isOpen: false
     };
   }
+  componentWillMount() {
+    document.addEventListener('mousedown', this.handleClickOutside);
+  }
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClickOutside);
+  }
+
   toggle() {
     this.setState({
       isOpen: !this.state.isOpen
     });
+  }
+  close() {
+    this.setState({
+      isOpen: false
+    });
+  }
+  handleClickOutside(event) {
+    const t = event.target;
+    if (this.state.isOpen
+      && !t.classList.contains('navbar-toggler')
+      && !t.classList.contains('nav-item')
+      && !t.classList.contains('navbar-nav')
+      && !t.classList.contains('nav-link')) {
+      this.close();
+    }
   }
   render() {
     return(
@@ -30,22 +54,22 @@ export default class GlobalNavBar extends Component {
         <Collapse isOpen={this.state.isOpen} navbar className="nav-center justify-content-center">
           <Nav navbar>
             <NavItem>
-              <NavLink href="#home">HOME</NavLink>
+              <NavLink onClick={this.close} href="#home">HOME</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="#about">ABOUT</NavLink>
+              <NavLink onClick={this.close} href="#about">ABOUT</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="#themes">THEMES</NavLink>
+              <NavLink onClick={this.close} href="#themes">THEMES</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="#faq">FAQ</NavLink>
+              <NavLink onClick={this.close} href="#faq">FAQ</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="#partners">PARTNERS</NavLink>
+              <NavLink onClick={this.close} href="#partners">PARTNERS</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="#sponsors">SPONSORS</NavLink>
+              <NavLink onClick={this.close} href="#sponsors">SPONSORS</NavLink>
             </NavItem>
           </Nav>
 
