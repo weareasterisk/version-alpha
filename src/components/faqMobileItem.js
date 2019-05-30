66f1f2f49b8fe152fe62b731eb4ff3f1a277f65d
Muhammad Khatri <muhammad@weareasterisk.com>
2019-05-29 23:09:59 -0500

    Fixed css for FAQ

--- src/components/faqMobileItem.js
+++ src/components/faqMobileItem.js
@@ -26,7 +26,7 @@ export default class FAQMobileItem extends Component {
   render() {
     return(
       <div>
-        <div className="w-600p margin-center container-fluid faq-content" onClick={this.toggle}>
+        <div className="w-max-600p margin-center container-fluid faq-content p-b-6" onClick={this.toggle}>
           <p className="font-weight-bold branding-orange m-b-8" onClick={this.toggle}> {this.props.faq.question} ></p>
           <Collapse isOpen={this.state.isOpen}>
             {ReactHtmlParser(this.props.faq.questionText)}
