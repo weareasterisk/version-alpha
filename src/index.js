1931dd6994c60f7b04411a0d732ee92b30a1bb76
Muhammad Khatri <muhammad@weareasterisk.com>
2019-06-28 22:43:08 -0500

    Updated project to deploy with react-snap for seo

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = document.getElementById('root');
+
+root.hasChildNodes() ?
+  ReactDOM.hydrate(<App />, root) :
+  ReactDOM.render(<App/>, root);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
